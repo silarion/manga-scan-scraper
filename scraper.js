ae053e39142ee2c0eccd3c869f5cc165691ac2bb
Arnaud <arnaud.gillet@intech.lu>
2016-10-02 12:15:19 +0200

    japanread scrapper

---
+++ scraper.js
@@ -0,0 +1,298 @@
+var request = require('request');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var decode = require('common-utils/base64').decode;
+var JSZip = require("jszip");
+var cheerio = require('cheerio');
+var XRegExp = require('xregexp').XRegExp;
+var settings = require('./configuration');
+var moment = require('moment');
+moment.locale('fr')
+
+var urls = [
+	'http://lel-scan.me/lecture-fairy-tail/490',
+	'http://www.japanread.net/manga/nanatsu-no-taizai/181',
+	'http://www.japanread.net/manga/hunter-x-hunter/359',
+	'http://www.japanread.net/manga/magi-the-labyrinth-of-magic/311',
+	'http://www.japanread.net/manga/claymore/156',
+	'http://www.japanread.net/manga/berserk/344',
+	'http://www.japanread.net/manga/One-punch-man/88.3',
+	'http://www.japanread.net/manga/one-piece/760',
+	'http://www.japanread.net/manga/gto-paradise-lost/1'
+];
+
+urls = [urls[8]];
+
+var regexURL = XRegExp('^(?<scheme> [^:/?]+ ) ://   # aka protocol   \n\
+			  (?<host>   [^/?]+  )       # domain name/IP \n\
+			  (?<path>   [^?]*   ) \\??  # optional path  \n\
+			  (?<query>  .*      )       # optional query', 'x');
+
+urls.forEach(function(url)
+{
+	
+	var host = XRegExp.exec(url, regexURL).host;
+	console.log("Host : " + host)
+	search(url, host);
+});
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+var oldDossier = null;
+
+var debug = false;
+
+var zip = new JSZip();
+
+function search(url, host) {
+	console.log("---------------------------------------------------------");
+	console.log('URL : ', url);
+	var manga = getManga(host, url);
+	
+    request(url, getRequestSettings(host), function (error, response, html) {
+
+        if (error) {
+            console.log('error', url, error);
+        } else {
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            var $ = cheerio.load(
+                html,
+                settings.cheerio
+            );
+
+            
+
+            var image = getImage($, host);
+			
+			if(!image){
+				//fin
+				//zip
+				if(!debug && oldDossier){
+					zip.generateNodeStream({type:'nodebuffer',streamFiles:true})
+						.pipe(fs.createWriteStream('K:\\Mangas\\' + manga + '\\Scans ' + manga + '\\' + manga + ' ' + oldDossier + '.cbz'))
+						.on('finish', function () {
+							// JSZip generates a readable stream with a "end" event,
+							// but is piped here in a writable stream which emits a "finish" event.
+							console.log(manga + ' ' + oldDossier + '.cbz' + " written.");
+						});
+					
+					//pour archive chapitre suivant
+					zip = new JSZip();
+				}
+				return;
+			}
+			
+			var match = XRegExp.exec(url, getRegex(host, url));
+			
+			var dossier = match[1];
+			if(dossier.indexOf('.') >= 0)
+				dossier = (Array(4).join('0') + dossier.substring(0, dossier.indexOf('.'))).slice(-3) + dossier.substring(dossier.indexOf('.'));
+			else if(dossier.length < 3)
+				dossier = (Array(4).join('0') + dossier).slice(-3);
+            console.log("Dossier : " + dossier)
+			
+			if(oldDossier != null && oldDossier != dossier){
+				//zip
+				if(!debug){
+					zip.generateNodeStream({type:'nodebuffer',streamFiles:true})
+						.pipe(fs.createWriteStream('K:\\Mangas\\' + manga + '\\Scans ' + manga + '\\' + manga + ' ' + oldDossier + '.cbz'))
+						.on('finish', function () {
+							// JSZip generates a readable stream with a "end" event,
+							// but is piped here in a writable stream which emits a "finish" event.
+							console.log(manga + ' ' + oldDossier + '.cbz' + " written.");
+						});
+					
+					//pour archive chapitre suivant
+					zip = new JSZip();
+				}
+			}
+			
+			oldDossier = dossier;
+			
+			var page = match[2];
+			if(page)
+				page = (Array(3).join('0') + page).slice(-2)
+			else
+				page = '00';
+            console.log("Page : " + page);
+			
+			//var ext = match[3];
+			var ext = /\.(\w+)$/.exec(image)[1];
+            console.log("Extension : " + ext);
+			
+			//stockage image
+			var dl = 'http://' + host + image;
+			if(image.indexOf("http:") >= 0){
+				dl = image;
+			}
+			console.log("Downloading : " + dl);
+			
+			var dir = 'K:\\Mangas\\' + manga + '\\Scans ' + manga + '\\' + dossier;
+			mkdirp(dir, function(err) { 
+				console.log(err);
+			});
+			
+			var file = dir + '\\' + page + '.' + ext;
+			
+			if(!debug){
+				download(dl, file, function(){
+					console.log(file + ' written');
+					
+					// read a file and add it to a zip
+					fs.readFile(file, function(err, data) {
+					  if (err) throw err;
+					  //var zip = new JSZip();
+					  zip.folder(manga + ' ' + dossier).file(page + '.' + ext, data);
+					  
+					  var nextPage = getPageSuivante($, host);
+						if(nextPage){
+							search(nextPage, host)
+						}
+					});
+					
+				});
+			}else{
+				var nextPage = getPageSuivante($, host);
+				if(nextPage){
+					search(nextPage, host)
+				}
+			}
+			
+			
+        }
+    });
+
+}
+
+page = 1;
+
+function getRegex(host, url) {
+    var regex = "regex";
+	
+    /*if(url.indexOf("lecture-fairy-tail") > 0){
+        regex = /\/(\d+(?:\.\d+)?)\/(\d+(?:-\d+)?)\.(\w{3})\?.*$/;
+    }
+	
+	if(host == "www.japanread.net"){
+        //regex = /\/(\d+(?:\.\d+)?)\/(\d+(?:-\d+)?)\/.*?\.(\w{3})$/;
+		//regex = /\/\d+(?:\.\d+)?\/(\d+(?:[.-]\d+)?)\/(?:.*?_(\d+)[^.]*|.*?(\d+))\.(\w{3})$/;
+		regex = /\/\d+(?:\.\d+)?\/([^\/]*?)\/.*?(\d+)[^\d]*\.(\w{3})$/;
+    }*/
+	
+	//regex = /[^\/]+?\/([\d._-]+?)(?:\/([^\/]+?)|)\/?$/;
+	regex = new RegExp("^.+?/([\\d._-]+)(?:/([\\d._-]+)|)/?$");
+	
+    console.log("Regex : " + regex)
+    return regex;
+}
+
+function getManga(host, url) {
+    var manga = "manga";
+	
+    if(url.indexOf("lecture-fairy-tail") > 0){
+        manga = "Fairy Tail";
+    }
+	
+	if(url.indexOf("nanatsu-no-taizai") > 0){
+        manga = "Seven Deadly Sins";
+    }
+	
+	if(url.indexOf("hunter-x-hunter") > 0){
+        manga = "HunterXHunter";
+    }
+	
+	if(url.indexOf("magi-the-labyrinth-of-magic") > 0){
+        manga = "Magi Labyrinth of Magic";
+    }
+	
+	if(url.indexOf("claymore") > 0){
+        manga = "Claymore";
+    }
+	
+	if(url.indexOf("berserk") > 0){
+        manga = "Berserk";
+    }
+	
+	if(url.indexOf("One-punch-man") > 0){
+        manga = "One Punch Man";
+    }
+	
+	if(url.indexOf("one-piece") > 0){
+        manga = "One Piece";
+    }
+	
+	if(url.indexOf("gto-paradise-lost") > 0){
+        manga = "GTO Paradise Lost";
+    }
+	
+
+    console.log("Manga : " + manga)
+    return manga;
+}
+
+/*cherche la page suivante de la recherche - sinon null*/
+function getPageSuivante($, host) {
+    var lienPageSuivante = null;
+    switch(host){
+        case "lel-scan.me" :
+            lienPageSuivante = $('a[href^="http://lel-scan.me/lecture-fairy-tail/"]:contains("Suiv")');
+            break;
+			
+		case "www.japanread.net" :
+			var image = getImage($, host);
+			lienPageSuivante = $('img[src="http://' + host + image + '"]').parent();
+			break;
+    }
+
+    lienPageSuivante = lienPageSuivante ? (lienPageSuivante.attr ? lienPageSuivante.attr('href') : lienPageSuivante.href) : null;
+    if(lienPageSuivante && lienPageSuivante.indexOf('http://') < 0){
+        lienPageSuivante = 'http://' + host + lienPageSuivante
+    }
+    console.log("Page suivante : " + lienPageSuivante)
+    return lienPageSuivante;
+}
+
+/*annonces de la page de recherche*/
+function getImage($, host) {
+	var image = null;
+    switch(host){
+        case "lel-scan.me" :
+            image = $('div[id="image"] a[href^="http://lel-scan.me/lecture-fairy-tail/"] img').attr('src');
+            break;
+			
+		case "www.japanread.net" :
+			image = $('img#img_mng_enl').attr('src');
+			break;
+    }
+	if(image){
+		if(image.indexOf('http://') < 0){
+			image = 'http://' + host + image;
+		}
+		console.log("Image src : " + image);
+		image = XRegExp.exec(image, regexURL).path;
+		console.log("Image : " + image);
+	}else{
+		console.log("Fin !");
+	}
+    return image;
+}
+
+function getRequestSettings(host) {
+    switch(host){
+        case "www.leboncoin.fr" :
+            settings.request.encoding = 'binary';
+            break;
+        default :
+            settings.request.encoding = 'utf8';
+            break;
+    }
+    return settings.request;
+}
